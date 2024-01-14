8e187799c87431479aa8eba9a1979a56bfc7a461
datadevbackyard <156578058+datadevbackyard@users.noreply.github.com>
2024-01-14 19:43:19 +0100

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "region_var" {
+
+    type = string 
+    default = "us-east-1"
+  
+}
+
+variable "profile_var" {
+    type = string 
+    default = "demo_aws"
+  
+}
+
+variable "bucket_name_var" {
+    type = string 
+    default = "demos3bucketusingterraform"
+  
+}
