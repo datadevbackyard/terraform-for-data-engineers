b078d053615e5178e33f5673d33b095bc36b7ba8
datadevbackyard <156578058+datadevbackyard@users.noreply.github.com>
2024-01-14 18:00:55 +0100

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,7 +1,24 @@
 locals {
-
-    region_name = "us-east-1"
-    profile_name = "demo_aws"
-    project_type = "demo"
   
+    s3_bucket_names = toset(["demos3bucketusingterraform1","demos3bucketusingterraform2","demos3bucketusingterraform3"])
+
+    s3_bucket_configs = {
+
+        demos3bucketusingterraform1 = {
+            versioning = true
+            project_type = "demo1"
+        }
+
+        demos3bucketusingterraform2 = {
+            versioning = false 
+            project_type = "demo2"
+        }
+
+        demos3bucketusingterraform3 = {
+            versioning = true
+            project_type = "demo3"
+        }
+
+
+    }
 }
